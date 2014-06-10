f7acdc9f6bc54bfef4fdc73ef9ff474c2b9a625a
Saheb Preet Singh <saheb.preet@gmail.com>
2014-06-10 14:58:14 +0530

    latest code

--- js/jquery.carousel.fullscreen.js
+++ js/jquery.carousel.fullscreen.js
@@ -1,8 +1,3 @@
-/******************************************************************************
-	Transforms the basic Twitter Bootstrap Carousel into Fullscreen Mode
-	@author Fabio Mangolini
-     http://www.responsivewebmobile.com
-******************************************************************************/
 jQuery(document).ready(function() {
 	$('.carousel').carousel({
     	pause: "false",
